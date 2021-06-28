08be7133c496a448eb7667c471c08c4ed49745eb
Greg van Berkel <gvanberkel@gmail.com>
2021-06-28 13:23:36 +0200

    some basic PWA support

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:greg_van_berkel/constants/routes.dart';
+
+import 'home_logic.dart';
 
 class HomeScreen extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ScopedReader watch) {
+    final logic = watch(homeScreenProvider);
+
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 0,
@@ -15,10 +20,24 @@ class HomeScreen extends ConsumerWidget {
             left: 16.0,
             right: 16.0,
           ),
-          child: Column(
-            children: [
-              Text('Please request a specific version of this document'),
-            ],
+          child: Center(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text('Please request a specific version of this document.'),
+                Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                        'Alternatively you can visit the most recent generic version.'),
+                    TextButton(
+                      onPressed: () => logic.navigate(Routes.g21),
+                      child: Text('here'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/home/home_logic.dart
+++ lib/screens/home/home_logic.dart
@@ -1,10 +1,17 @@
+import 'dart:async';
+
 import 'package:flutter_flicker/flutter_flicker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:greg_van_berkel/constants/routes.dart';
 
 class HomeScreenLogic extends BaseLogic {
   @override
   Future<void> init() async {
     notifyScreen(true);
+  }
+
+  void goToCurrentVersion() {
+    this.navigate(Routes.g21);
   }
 }
 
